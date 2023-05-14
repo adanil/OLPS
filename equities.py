c9b24900a42d12902b08c013557133fbcbab7ef4
Stepan <69470605+Rheamer@users.noreply.github.com>
2023-05-14 15:51:55 +0300

    Create equities.py

---
+++ equities.py
@@ -0,0 +1,19 @@
+#region imports
+from AlgorithmImports import *
+#endregion
+
+
+MichaelBurrySymbols = ["BKI","GEO","WWW","BABA","COHR","MGM","JD","SKYW","QRTEA"]
+
+WarrenBuffetSymbols = ["AAPL","BAC","CVX","KO","AXP","KHC","OXY","MCO","TSM",
+"ATVI","HPQ","DVA","BK","USB","VRSN","VIAC","C","KR","V","GM","MA","AON","CHTR","FWONA",
+"MCK","AMZN","CE","SNOW","TMUS"]
+
+HowardMarksSymbols = ["TRMD","CHK","VST","SBLK","STR","RWAY","EGLE","WFRD","STKL",
+"INFN","ALLY","VALE","PCG","ITUB","AU","NMIH","FYBR","HTZ","FCX","CBL","IBN","PBR","KRC",
+"ALVO","CX","BAP","BRY","VIST","BBD","LBTYA","FTAI","OCSL","AZUL","BATL","TOL","VVR",
+"PHM","GTX","VAL","PAM","CVT"]
+
+RussianMarketSymbols = ["IRAO","GAZP","SNGS","SNGSP","SBER","VTBR","ALRS","RUAL","MOEX",
+"NLMK","TATN","MTSS","ROSN","NVTK","LKOH","POLY","CHMF","VKCO","FIVE","YNDX","TCSG",
+"PHOR","MGNT","OZON","GMKN","PLZL",]
